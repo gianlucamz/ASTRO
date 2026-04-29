8c31d518bc43c32098ddfa9dca8d5ff14bde3605
gianlucamz <dd .>
2026-04-29 15:08:45 -0300

    fix: corrige caminho das rotas da home

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 
 import { IoLogoFacebook, IoLogoInstagram } from "react-icons/io5";
 import logo from "../assets/astroLogo.png";
