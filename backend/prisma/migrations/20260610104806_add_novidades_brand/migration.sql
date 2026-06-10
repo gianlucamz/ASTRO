d3e054e5fcb9dd89c14113d0fb84aaaecdeee365
gianlucamz <dd .>
2026-06-10 08:05:25 -0300

    alter table product

--- backend/prisma/migrations/20260610104806_add_novidades_brand/migration.sql
+++ backend/prisma/migrations/20260610104806_add_novidades_brand/migration.sql
@@ -1,3 +1,3 @@
 -- AlterTable
-ALTER TABLE `product` ADD COLUMN `brand` VARCHAR(191) NULL,
+ALTER TABLE `Product` ADD COLUMN `brand` VARCHAR(191) NULL,
     ADD COLUMN `novidades` BOOLEAN NOT NULL DEFAULT false;
